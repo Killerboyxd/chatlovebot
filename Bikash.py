7151c2322574b735ffd2594aa12ff69806de33d6
SHIVxOP <122870924+Killerboyxd@users.noreply.github.com>
2023-03-28 10:23:44 +0530

    Update Bikash.py

--- Bikash.py
+++ Bikash.py
@@ -39,15 +39,15 @@ async def is_admins(chat_id: int):
 @bot.on_message(filters.command("start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"{BOT_IMAGE}",
+        photo=f"https://telegra.ph/file/9584b19633bf5b31faa12.jpg",
         caption=f"""**━━━━━━━━━━━━━━━━━━━━━━━━
-𝑯𝒆𝒍𝒍𝒐🥀' 𝐈 𝐚𝐦 𝐀 𝐏𝐨𝐰𝐞𝐫𝐟𝐮𝐥𝐥 𝐂𝐡𝐚𝐭𝐛𝐨𝐭 𝐨𝐟 𝐭𝐞𝐥𝐞𝐠𝐫𝐚𝐦 .\n\n🥀 𝐌𝐲 𝐧𝐚𝐦𝐞 𝐢𝐬 {BOT_NAME} !\n\n /chatbot - [on|off] 𝐓𝐡𝐢𝐬 𝐂𝐨𝐦𝐦𝐚𝐧𝐝 𝐔𝐬𝐞 𝐎𝐧𝐥𝐲 𝐀𝐧𝐲 𝐆𝐫𝐨𝐮𝐩
+𝑯𝒆𝒍𝒍𝒐🥀' 𝐈 𝐚𝐦 𝐀 𝐏𝐨𝐰𝐞𝐫𝐟𝐮𝐥𝐥 𝐒𝐡𝐢𝐯 𝐂𝐡𝐚𝐭𝐛𝐨𝐭 𝐨𝐟 𝐭𝐞𝐥𝐞𝐠𝐫𝐚𝐦 .\n\n🥀 𝐌𝐲 𝐧𝐚𝐦𝐞 𝐢𝐬 {BOT_NAME} !\n\n /chatbot - [on|off] 𝐓𝐡𝐢𝐬 𝐂𝐨𝐦𝐦𝐚𝐧𝐝 𝐔𝐬𝐞 𝐎𝐧𝐥𝐲 𝐀𝐧𝐲 𝐆𝐫𝐨𝐮𝐩
 
 ┏━━━━━━━━━━━━━━━━━┓
-┣➤ 𝐎𝐰𝐧𝐞𝐫  ➪ [𝐂𝐥𝐢𝐜𝐤 𝐡𝐞𝐫𝐞](https://t.me/{OWNER_USERNAME})
-┣➤ 𝐔𝐩𝐝𝐚𝐭𝐞𝐬 ➪ [𝐖𝐂𝐅 𝐍𝐞𝐭𝐰𝐨𝐫𝐤](https://t.me/{UPDATES_CHANNEL})
-┣➤ 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 ➪ [𝐖𝐂𝐅 𝐎𝐏](https://t.me/{SUPPORT_GROUP})
-┣➤ 𝐂𝐫𝐞𝐚𝐭𝐨𝐫 ➪ [𝐋𝐞𝐠𝐞𝐧𝐝](https://t.me/Sangram_XD)
+┣➤ 𝐎𝐰𝐧𝐞𝐫  ➪ [𝐒𝐡𝐢𝐯 𝐎𝐩](https:/)t.me/ll_SABKA_BHAI_KILLER_ll)
+┣➤ 𝐔𝐩𝐝𝐚𝐭𝐞𝐬 ➪ [𝐒𝐡𝐢𝐯 𝐂𝐡𝐚𝐧𝐧𝐞𝐥](https://t.me/do_dil_ek_jaan143)
+┣➤ 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 ➪ [𝐑𝐨𝐧𝐧𝐲 𝐎𝐏](https://t.me/RONNY_KI_DUNIYA)
+┣➤ 𝐂𝐫𝐞𝐚𝐭𝐨𝐫 ➪ [𝐊𝐈𝐋𝐋𝐄𝐑 𝐁𝐎𝐘](https:/)t.me/ll_KILLER_BOY_OP_ll)
 ┗━━━━━━━━━━━━━━━━━┛
 
 ❣️ 𝐉𝐮𝐬𝐭 𝐀𝐝𝐝 𝐌𝐞 𝐓𝐨 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩 𝐀𝐧𝐝
@@ -69,13 +69,13 @@ async def start_(client: Client, message: Message):
 @bot.on_message(filters.command(["/start", f"start@{BOT_USERNAME}", "/alive", ".alive", "#Sangram", "Sangram", "sangram"]) & filters.group & ~filters.edited)
 async def start(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"{BOT_IMAGE}",
-        caption=f"""𝑯𝒆𝒍𝒍𝒐🥀' 𝐈 𝐚𝐦 𝐀 𝐏𝐨𝐰𝐞𝐫𝐟𝐮𝐥𝐥 𝐂𝐡𝐚𝐭𝐛𝐨𝐭 𝐨𝐟 𝐭𝐞𝐥𝐞𝐠𝐫𝐚𝐦.\n\n🥀 𝐌𝐲 𝐧𝐚𝐦𝐞 𝐢𝐬 {BOT_NAME} !\n\n🥀𝐀𝐧𝐲 𝐩𝐫𝐨𝐛𝐥𝐞𝐦 𝐭𝐨 [𝐑𝐞𝐩𝐨𝐫𝐭](https://t.me/{SUPPORT_GROUP})  🥀\n\n[𝐔𝐩𝐝𝐚𝐭𝐞𝐬](https://t.me/{UPDATES_CHANNEL}) 🥀\n\n /chatbot - [on|off]""",
+        photo=f"https://telegra.ph/file/9584b19633bf5b31faa12.jpg",
+        caption=f"""𝑯𝒆𝒍𝒍𝒐🥀' 𝐈 𝐚𝐦 𝐀 𝐏𝐨𝐰𝐞𝐫𝐟𝐮𝐥𝐥 𝐒𝐡𝐢𝐯 𝐂𝐡𝐚𝐭𝐛𝐨𝐭 𝐨𝐟 𝐭𝐞𝐥𝐞𝐠𝐫𝐚𝐦.\n\n🥀 𝐌𝐲 𝐧𝐚𝐦𝐞 𝐢𝐬 @SUCHI_BABY_BOT !\n\n🥀𝐀𝐧𝐲 𝐩𝐫𝐨𝐛𝐥𝐞𝐦 𝐭𝐨 [𝐑𝐞𝐩𝐨𝐫𝐭](https://t.me/seqime)  🥀\n\n[𝐔𝐩𝐝𝐚𝐭𝐞𝐬](https://t.me/do_dil_ek_jaan143) 🥀\n\n /chatbot - [on|off]""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        " 💝 𝐉𝐨𝐢𝐧 𝐎𝐮𝐫 𝐂𝐡𝐚𝐭 𝐆𝐫𝐨𝐮𝐩 💝", url=f"https://t.me/WorldChattingFriendsWCF")
+                        " 💝 𝐉𝐨𝐢𝐧 𝐎𝐮𝐫 𝐂𝐡𝐚𝐭 𝐆𝐫𝐨𝐮𝐩 💝", url=f"https://t.me/RONNY_KI_DUNIYA")
                 ]
             ]
         ),
